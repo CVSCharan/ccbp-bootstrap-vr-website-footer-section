0ef52446dee37da453b098793d69c491e1d737ee
CVSCharan <charan.cvs@gmail.com>
2022-08-07 19:21:25 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,59 +2,59 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="bg-container">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-md-5 section1">
-            <div class="col-12 mt-4 mb-2">
-              <div class="col-12 col-md-1 d-flex flex-row justify-content-center">
+    <div className="bg-container">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 section1">
+            <div className="col-12 mt-4 mb-2">
+              <div className="col-12 col-md-1 d-flex flex-row justify-content-center">
                 <img
                   src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/vr-logo-img.png"
-                  class="img"
+                  className="img"
                   alt="img1"
                 />
               </div>
             </div>
-            <div class="col-12">
-              <div class="iconscard d-flex flex-row justify-content-center">
-                <div class="iconscontainer d-flex flex-column justify-content-center">
-                  <i class="fab fa-google icon"></i>
+            <div className="col-12">
+              <div className="iconscard d-flex flex-row justify-content-center">
+                <div className="iconscontainer d-flex flex-column justify-content-center">
+                  <i className="fab fa-google icon"></i>
                 </div>
-                <div class="iconscontainer d-flex flex-column justify-content-center">
-                  <i class="fab fa-twitter icon"></i>
+                <div className="iconscontainer d-flex flex-column justify-content-center">
+                  <i className="fab fa-twitter icon"></i>
                 </div>
-                <div class="iconscontainer d-flex flex-column justify-content-center">
-                  <i class="fab fa-instagram icon"></i>
+                <div className="iconscontainer d-flex flex-column justify-content-center">
+                  <i className="fab fa-instagram icon"></i>
                 </div>
-                <div class="iconscontainer d-flex flex-column justify-content-center">
-                  <i class="fab fa-linkedin icon"></i>
+                <div className="iconscontainer d-flex flex-column justify-content-center">
+                  <i className="fab fa-linkedin icon"></i>
                 </div>
               </div>
             </div>
-            <div class="addsection text-center">
+            <div className="addsection text-center">
               37, Ayur Vigyan Nagar, New Delhi, INDIA.
             </div>
           </div>
 
-          <div class="col-5 col-md-3 mt-4 mb-4">
-            <div class="section2">
-              <h1 class="cardheading1">Contact Us</h1>
-              <div class="d-flex flex-row mb-2">
-                <i class="fas fa-envelope icon1"></i>
-                <p class="cardpara1">vr@gmail.com</p>
+          <div className="col-5 col-md-3 mt-4 mb-4">
+            <div className="section2">
+              <h1 className="cardheading1">Contact Us</h1>
+              <div className="d-flex flex-row mb-2">
+                <i className="fas fa-envelope icon1"></i>
+                <p className="cardpara1">vr@gmail.com</p>
               </div>
-              <div class="d-flex flex-row mb-2">
-                <i class="fas fa-phone-alt icon1"></i>
-                <p class="cardpara1">1234506789</p>
+              <div className="d-flex flex-row mb-2">
+                <i className="fas fa-phone-alt icon1"></i>
+                <p className="cardpara1">1234506789</p>
               </div>
-              <div class="d-flex flex-row">
-                <i class="fab fa-skype icon1"></i>
-                <p class="cardpara1">Rahul</p>
+              <div className="d-flex flex-row">
+                <i className="fab fa-skype icon1"></i>
+                <p className="cardpara1">Rahul</p>
               </div>
             </div>
           </div>
 
-          <div class="col-6 col-md-3 ml-4 mt-4 mb-4">
+          <div className="col-6 col-md-3 ml-4 mt-4 mb-4">
             <div class="section3">
               <h1 class="cardheading1">Let Us Help You</h1>
               <p class="cardpara2">100% Purchase</p>
